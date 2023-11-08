1cc709ab5af50f8a2de3feb05c7de8721e4b1618
G00394619@atu.ie <g00394619@atu.ie>
2023-11-08 09:35:01 +0000

    Starting OrderService

--- src/main/java/ie/atu/ordermanagement/OrderService.java
+++ src/main/java/ie/atu/ordermanagement/OrderService.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@Autowired
+
+
 
 public class OrderService {
-   public void createOrder (){
 
-   }
-public void getOrderById{
-
-}
 
 }
