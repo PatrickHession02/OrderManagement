a4985f4b88cc51ec7af0fbd9ad056126e3333ed5
G00394619@atu.ie <g00394619@atu.ie>
2023-11-08 09:28:53 +0000

    Added OrderService

---
+++ src/main/java/ie/atu/ordermanagement/OrderController.java
@@ -0,0 +1,37 @@
+package ie.atu.ordermanagement;
+
+import feign.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+
+public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+
+    }
+
+    @GetMapping ("/createOrder/{OrderID}")
+
+    public Response Entity <int> createOrder(@PathVariable int OrderID){
+        return null;
+    }
+
+    @PostMapping("/ceateOrderBody")
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public Order orderBody(@RequestBody Order order){
+
+
+    }
+
+
+}
---
+++ src/main/java/ie/atu/ordermanagement/OrderService.java
@@ -0,0 +1,17 @@
+package ie.atu.ordermanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Autowired
+
+public class OrderService {
+   public void createOrder (){
+
+   }
+public void getOrderById{
+
+}
+
+}
